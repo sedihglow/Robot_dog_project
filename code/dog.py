89e215198cf189f305b905bc5f92817bcdeeae85
sedihglow <jamesmross4@gmail.com>
2020-12-25 17:31:49 -0800

    added some functions to dog.py, not tested and might need to change turn_left and turn_right's range function

--- code/dog.py
+++ code/dog.py
@@ -1,4 +1,33 @@
 from Control import *
 from Action import *
 
+class dog:
+    def __init__(self):
+        self.control = Control()
+        self.action = Action()
+
+    # move forward x steps
+    def move_forward(self, steps):
+        for i in range(steps):
+            self.control.forWard()
+        self.control.stop()
+
+    #turn right x degrees 
+    # TODO: Test this, might need a different range and not sure with stop()
+    def turn_right(self, degree):
+        for i in range(degree):
+            self.control.attitude(0,0,i)
+            time.sleep(0.1)
+        self.control.stop()
+
+    # turn left x degrees
+    # TODO: Test this, might need a different range and not sure with stop()
+    def turn_left(self, degree):
+        for i in range(-degree):
+            self.control.attitude(0,0,i)
+            time.sleep(0.1)
+        self.control.stop()
+    
+    def push_ups(self):
+        self.action.push_ups()
 
